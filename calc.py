f08f568e9cbd857ec61f64c7be022cc00d12adab
Nes Cohen <gomariners1129@gmail.com>
2015-05-16 13:19:08 -0700

    added test result

--- calc.py
+++ calc.py
@@ -7,37 +7,58 @@ def getCrimeData():
 	results = smart.data("PDX Crime Data 2013", {"limit": 100})
 	print len(results)
 	count = 0;
-	morning = 0;
-	afternoon = 0;
-	night = 0;
-	violent = 0;
+	morningV = 0;
+	morningNV = 0;
+	afternoonV = 0;
+	afternoonNV = 0;
+	nightV = 0;
+	nightNV = 0;
 	for i in xrange(0, len(results)):
 		#if results[i]["X Coordinate"] <= 42.996 and results[i]["X Coordinate"] >= 42.796 and results[i]["Y Coordinate"] >= -122.236 and results[i]["Y Coordinate"] <= -122.036:
 		count += 1;
 		if int(results[i]["Report Time"][:2]) < 8 or int(results[i]["Report Time"][:2]) >= 19:
-			night += 1;
-		if int(results[i]["Report Time"][:2]) <= 12 and int(results[i]["Report Time"][:2]) >= 8:
-			morning += 1;
-		if int(results[i]["Report Time"][:2]) < 19 and int(results[i]["Report Time"][:2]) > 12:
-			afternoon += 1;
-		#print results[i]["Report Time"]
-		if "Assault" in results[i]["Major Offense Type"] or \
+			if "Assault" in results[i]["Major Offense Type"] or \
 			"Homicide" in results[i]["Major Offense Type"] or \
 			"Rape" in results[i]["Major Offense Type"] or \
 			"Kidnap" in results[i]["Major Offense Type"] or \
 			"Robbery" in results[i]["Major Offense Type"] or \
 			"Arson" in results[i]["Major Offense Type"]:
-			violent += 1;
-	print "total:", count
-	print "morning:", morning
-	print "afternoon:", afternoon
-	print "night:", night
-	print "violent:", violent
+				nightV += 1;
+			else: nightNV += 1
+		if int(results[i]["Report Time"][:2]) <= 12 and int(results[i]["Report Time"][:2]) >= 8:
+			if "Assault" in results[i]["Major Offense Type"] or \
+			"Homicide" in results[i]["Major Offense Type"] or \
+			"Rape" in results[i]["Major Offense Type"] or \
+			"Kidnap" in results[i]["Major Offense Type"] or \
+			"Robbery" in results[i]["Major Offense Type"] or \
+			"Arson" in results[i]["Major Offense Type"]:
+				morningV += 1;
+			else: morningNV += 1
+		if int(results[i]["Report Time"][:2]) < 19 and int(results[i]["Report Time"][:2]) > 12:
+			if "Assault" in results[i]["Major Offense Type"] or \
+			"Homicide" in results[i]["Major Offense Type"] or \
+			"Rape" in results[i]["Major Offense Type"] or \
+			"Kidnap" in results[i]["Major Offense Type"] or \
+			"Robbery" in results[i]["Major Offense Type"] or \
+			"Arson" in results[i]["Major Offense Type"]:
+				afternoonV += 1;
+			else: afternoonNV += 1
+		#print results[i]["Report Time"]
+	print "total:    ", count
+	print "           V - NV"
+	print "morning:  ", morningV, " ", morningNV
+	print "afternoon:", afternoonV, " ", afternoonNV
+	print "night:    ", nightV, " ", nightNV
+	result[6] = {morningV, morningNV, afternoonV, afternoonNV, nightV, nightNV};
+	return result;
 
 
 def crime(station, time):
-	value = 0;
-	return {"score":value} #score
+	results = getCrimeData();
+	return {"morning":{"violent": results[0], "nonviolent": results[1]}
+			"afternoon":{"violent": results[2],"nonviolent": results[3]}
+			"night":{"violent": results[4],"nonviolent": results[5]}
+			} #score
 
 def main():
 	getCrimeData();
